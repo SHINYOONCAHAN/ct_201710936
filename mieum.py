707aaaf807cd52ee9dac1b82b1e6e349732e35b1
SHINYOONCHAN <sinsyc28@naver.com>
2017-05-22 17:01:27 +0900

    Add files via upload

---
+++ mieum.py
@@ -0,0 +1,26 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+def giyukAt(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    t1.fd(size)
+    t1.right(90)
+    t1.fd(size)
+def nieunAt(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    t1.right(90)
+    t1.fd(size)
+    t1.left(90)
+    t1.fd(size)
+def mieumAt(x,y,size):
+    giyukAt(x,y,size)
+    t1.penup()
+    t1.home()
+    t1.pendown()
+    nieunAt(x,y,size)
+mieumAt(100,100,50)
+wn.exitonclick()
